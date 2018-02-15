c52b5a9db949282e6cd55c9e550d718a9dbbf607
Daniel Reed <daniel@danielreed.org>
2018-02-14 22:05:48 -0400

    Initial commit

---
+++ R/style.R
@@ -0,0 +1,19 @@
+####################################################################
+# Author: Daniel Reed (daniel@danielreed.org)
+# Purpose: Style functions
+# Date: Wed Feb 14 21:37:21 2018
+####################################################################
+
+#' Basic theme for ggplot2 figures
+#'
+#' @param ... Arguments passed to \code{theme} function of \code{ggplot2}
+#' @seealso \code{\link{ggplot2}{theme}}
+#' @export
+theme_workhorse <- function(...){
+  ggplot2::theme(text = ggplot2::element_text(family = "serif"),
+                 panel.background = ggplot2::element_blank(),
+                 panel.border = ggplot2::element_rect(fill = NA, size = 1),
+                 axis.title = ggplot2::element_text(size = 24),
+                 axis.text = ggplot2::element_text(size = 16)
+                 ) + ggplot2::theme(...)
+}
