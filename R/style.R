15fdfe7ad691a57180226ec2f33bfaed5cd26c8e
Daniel Reed <daniel@danielreed.org>
2018-11-10 00:02:12 -0400

    Shephard Fairey colour palette added

--- R/style.R
+++ R/style.R
@@ -14,6 +14,38 @@ theme_ocean <- function(...){
                  panel.background = ggplot2::element_blank(),
                  panel.border = ggplot2::element_rect(fill = NA, size = 1),
                  axis.title = ggplot2::element_text(size = 24),
-                 axis.text = ggplot2::element_text(size = 16)
+                 axis.text = ggplot2::element_text(size = 16),
+                 panel.grid.major = ggplot2::element_line(colour = scales::alpha(gray50, 0.5),
+                                                          linetype = "dashed")
                  ) + ggplot2::theme(...)
+}
+
+#' Shepard Fairey colour palette
+#'
+#' @param num Number of colours required
+#' @param type \code{discrete} or \code{continuous} colours
+#' @return Returns colours palette with \code{num} colours
+#' @export
+fairey <- function(num, type = c("discrete", "continuous")) {
+  type <- match.arg(type)
+
+  # Colours
+  pal <- c("#0A2A3F", "#6593A0", "#B9CCB8", "#FFEFA7", "#DB1522")
+
+  # If number of colours missing use them all
+  if(missing(num)) {
+    num <- length(pal)
+  }
+
+  # If there are fewer colours than requested STOP
+  if(type == "discrete" & num > length(pal)) {
+    stop("Number of requested colors greater than what palette can offer")
+  }
+
+  # If continuous, interpolate between colours
+  out <- switch(type,
+                continuous = grDevices::colorRampPalette(pal)(num),
+                discrete = pal[1:num]
+  )
+  structure(out, class = "palette")
 }
