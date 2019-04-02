95a7f37d754b13c25772e90775d20639436d69e6
Dan Reed <daniel@danielreed.org>
2019-04-02 12:07:55 -0300

    Check functions added

--- R/utility.R
+++ R/utility.R
@@ -59,3 +59,59 @@ conditionally <- function(fun){
     else return(first_arg)
   }
 }
+
+#' Check factor column
+#' @param x factor
+check.factor <- function(x){
+  if(!is.factor(x))stop("Nope – not a factor")
+  message("Factor with levels ", paste(levels(x), collapse = ","))
+  message(sum(!is.na(x)), " values, ", sum(is.na(x)), " NAs (", length(x), " total)")
+}
+
+#' Check numeric column
+#' @param x numeric
+#' @return No return values - a box/dotplot is produced to examine values
+check.numeric <- function(x){
+  if(!is.numeric(x))stop("Feed me numerics not ", class(x), "s")
+
+  message("See plot...")
+
+  ggplot2::ggplot(data.frame(y = x), aes(x = 1, y = y)) +
+    ggplot2::geom_boxplot() +
+    ggplot2::geom_dotplot(binaxis = "y", stackdir = "center") +
+    ggplot2::ylab("Values") +
+    ggplot2::xlab("") +
+    kitchendraw::theme_ocean(axis.text.x = ggplot2::element_blank())
+}
+
+#' Check character column
+#' @param x numeric
+#' @param n number of random values to show – 20 by default
+check.character <- function(x, n = 20){
+  if(!is.character(x))stop("Expected characters, got ", class(x), "s. Disappointed.")
+  message("Character with ", length(unique(x)), " unique values")
+  message(sum(!is.na(x)), " values, ", sum(is.na(x)), " NAs (", length(x), " total)")
+  message("Random sample of values:\n", paste(" ", sample(x, min(length(x), n)), "\n", sep = ""))
+}
+
+#' Dispatch function for checking
+#' @param variable to be 'checked'
+check <- function(x, ...){
+  UseMethod("check", x)
+}
+
+#' Summarise each column of a data frame
+#'
+#' @param df The data frame to be summarised
+#' @return Returns \code{NULL}, but outputs figures and summary tables
+#' @export
+check_df <- function(df){
+  # Check that df is a data frame
+  if(!is.data.frame(df))stop("What are you trying to pull here?! check_df expects a data frame")
+
+  # Get column names
+  df_cols <- colnames(df)
+
+  # Check all columns
+  invisible(lapply(df_cols, function(x){print(x, quote = FALSE); check(df[[x]])}))
+}
