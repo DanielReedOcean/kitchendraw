b2910ed1b8791b402bb771a1403f129dc07569df
Dan Reed <daniel@danielreed.org>
2019-06-25 14:44:14 -0300

    Removed obsolete functions and added negated %in% function

--- R/utility.R
+++ R/utility.R
@@ -28,94 +28,9 @@ strip <- function(vec){
   vec
 }
 
-#' Calculate proportions of two water masses in a third (target) water mass using a conservative tracer.
+#' Not in operator. Negated \code{%in%}.
 #'
-#' @param t1 Tracer in first water mass
-#' @param t2 Tracer in second water mass
-#' @param tT Tracer of target water mass
-#' @return Return a data frame containing proportion of two water masses calculated from temperature & salinity.
-#' @export
-wm <- function(t1, t2, tT){
-  alpha <- (tT - t2)/(t1 - t2)
-
-  return(data.frame(alpha = alpha, beta =  1 - alpha))
-}
-
-
-#' Convert a function into a conditional function
-#'
-#' @param fun Name of function to convert
-#' @return Function \code{fun} with an additional logical parameter \code{execute} that toggles whether it is
-#' executed or not.
-#'
-#' @description This function takes another function \code{fun} and adds a logical parameter called \code{execute} that
-#' that specifies whether the function is executed or not. The function was created by \emph{ablack3} and posted
-#' \href{https://github.com/tidyverse/magrittr/issues/109#issuecomment-371999796}{here} on GitHub.
-#'
-#' @export
-conditionally <- function(fun){
-  function(first_arg, ..., execute){
-    if(execute) return(fun(first_arg, ...))
-    else return(first_arg)
-  }
-}
-
-#' Check factor column
-#' @param x factor
-check.factor <- function(x, x_name){
-  if(!is.factor(x))stop("Nope – not a factor")
-  message(x_name, ":")
-  message("\tFactor with levels ", paste(levels(x), collapse = ","))
-  message("\t", sum(!is.na(x)), " values, ", sum(is.na(x)), " NAs (", length(x), " total)")
-}
-
-#' Check numeric column
-#' @param x numeric
-#' @return No return values - a box/dotplot is produced to examine values
-check.numeric <- function(x, x_name){
-  if(!is.numeric(x))stop("Feed me numerics not ", class(x), "s")
-
-  message(x_name, ":")
-  message("\tNumeric with ", sum(!is.na(x)), " values, ", sum(is.na(x)), " NAs (", length(x), " total)")
-  message("\tRange: ", paste(signif(range(x, na.rm =TRUE)), collapse = "-"))
-  message("\tMean: ", signif(mean(x, na.rm =TRUE)))
-  message("\tMedian: ", signif(median(x, na.rm =TRUE)))
-  message("\tSee plot...")
-
-  suppressMessages(print(ggplot2::ggplot(data.frame(y = x), ggplot2::aes(x = 1, y = y)) +
-    ggplot2::geom_violin(colour = NA, fill = scales::alpha("#FF3300", 0.35)) +
-    ggplot2::ylab("Values") +
-    ggplot2::xlab("") +
-    kitchendraw::theme_ocean(axis.text.x = ggplot2::element_blank()) +
-    ggplot2::ggtitle((x_name))))
-}
-
-#' Check character column
-#' @param x numeric
-#' @param n number of random values to show – 20 by default
-check.character <- function(x, x_name, n = 20){
-  if(!is.character(x))stop("Expected characters, got ", class(x), "s. Disappointed.")
-  message(x_name, ":")
-  message("\tCharacter with ", length(unique(x)), " unique values")
-  message("\t", sum(!is.na(x)), " values, ", sum(is.na(x)), " NAs (", length(x), " total)")
-  message("\tRandom sample of values:\n", paste("\t", sample(x, min(length(x), n)), "\n", sep = ""))
-}
-
-#' Dispatch function for checking
-#' @param variable to be 'checked'
-check <- function(x, ...){
-  UseMethod("check", x)
-}
-
-#' Summarise each column of a data frame
-#'
-#' @param df The data frame to be summarised
-#' @return Returns \code{NULL}, but outputs figures and summary tables
-#' @export
-check_df <- function(df){
-  # Check that df is a data frame
-  if(!is.data.frame(df))stop("What are you trying to pull here?! check_df expects a data frame")
-
-  # Check all columns
-  invisible(lapply(colnames(df), function(i)check(x = df[[i]], x_name = i)))
-}
+#' @usage x %!in% y
+#' @param x Vector
+#' @param y Vector
+`%!in%` <- Negate("%in%")
